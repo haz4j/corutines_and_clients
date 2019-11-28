2511e09d8e1503d86d66f1f201ac9eae9c42ec79
Nikolay.Petrusenko <saltpan@gmail.com>
2019-11-28 16:48:29 +0300

    blocks

--- src/main/kotlin/com.haz.MainApp.kt
+++ src/main/kotlin/com.haz.MainApp.kt
@@ -1,27 +1,38 @@
-
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClients
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import kotlin.system.measureTimeMillis
 
-
-fun main() {
-    val launch = GlobalScope.launch {
-        // launch a new coroutine in background and continue
-        launchClient()
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-        println("World!") // print after delay
+fun main() = runBlocking<Unit> {
+    val time = measureTimeMillis {
+        val one = async { doSomethingUsefulOne() }
+        val two = async { doSomethingUsefulTwo() }
+        println("The answer is ${one.await() + two.await()}")
     }
-    println("Hello,") // main thread continues while coroutine is delayed
-    Thread.sleep(5000L) // block main thread for 2 seconds to keep JVM alive
+    println("Completed in $time ms")
+}
 
-    launchClient()
+suspend fun doSomethingUsefulOne(): Int {
+    println("doSomethingUsefulOne before")
+//    launchClient()
+    delay(1000L) // pretend we are doing something useful here
+    println("doSomethingUsefulOne after")
+    return 13
+}
+
+suspend fun doSomethingUsefulTwo(): Int {
+    println("doSomethingUsefulTwo before")
+//    launchClient()
+    delay(1000L) // pretend we are doing something useful here, too
+    println("doSomethingUsefulTwo after")
+    return 29
 }
 
 private fun launchClient() {
+
+
     val client = HttpClients.createDefault()
     val httpGet = HttpGet("http://www.ya.ru")
     val response = client.execute(httpGet)
@@ -35,5 +46,5 @@ private fun launchClient() {
     }
 
 //    println(textView.toString())
-    println("client finished")
+//    println("client finished")
 }
